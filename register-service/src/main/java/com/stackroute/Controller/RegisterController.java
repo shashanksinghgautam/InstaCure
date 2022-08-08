cff60f40c4992a49099651f295a581264d43fbb2
shashanksinghgautam <60248212+shashanksinghgautam@users.noreply.github.com>
2022-08-08 18:00:41 +0530

    Registration coded

---
+++ register-service/src/main/java/com/stackroute/Controller/RegisterController.java
@@ -0,0 +1,17 @@
+package com.stackroute.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/register")
+public class RegisterController {
+
+}
+
