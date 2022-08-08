ff04d2a2f8bed85e7e1153a4052ba95527a9119f
shashanksinghgautam <60248212+shashanksinghgautam@users.noreply.github.com>
2022-08-08 18:26:22 +0530

    Registration coded-3

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { PatientComponentComponent } from './patient-component/patient-component
     LoginComponentComponent,
     RegisterComponentComponent,
     PatientComponentComponent,
+    VolunteerProfileComponent
   ],
   imports: [
     BrowserModule,
