5395d1b0b656f2db9d25d5d62e9a424339b458cc
Karthika-Rajesh <karthikarajesh006@gmail.com>
2022-08-08 13:45:07 +0530

    Added patient profile UI

---
+++ product-webapp/webapp/src/app/patient-component/patient-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-component',
+  templateUrl: './patient-component.component.html',
+  styleUrls: ['./patient-component.component.css']
+})
+export class PatientComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
