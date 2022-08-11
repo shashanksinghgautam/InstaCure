90d00fee322464412a28be96c188cd873c204677
shashanksinghgautam <60248212+shashanksinghgautam@users.noreply.github.com>
2022-08-11 09:50:44 +0530

    Login/Regis connected

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -8,46 +8,31 @@ import { RegisterComponentComponent } from './register-component/register-compon
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-=======
 import { VolunteerProfileComponent } from './volunteer-profile/volunteer-profile.component';
->>>>>>> e7a3f3b503a5e201e706cf58f9860de2facd41c7
 import { PatientComponentComponent } from './patient-component/patient-component.component';
-import { HttpClientModule,HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { medicineComponent } from './medicine/medicine.component';
-<<<<<<< HEAD
 import { VolunteerComponent } from './medicine/volunteer/volunteer.component';
 import { UpdateComponent } from './medicine/volunteer/update/update.component';
-=======
+
 import { HomeComponent } from './home/home.component';
 import { PatientComponent } from './consultation/patient/patient.component';
-
-
->>>>>>> e7a3f3b503a5e201e706cf58f9860de2facd41c7
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponentComponent,
     RegisterComponentComponent,
-<<<<<<< HEAD
     PatientComponentComponent,
     VolunteerProfileComponent,
     medicineComponent,
     VolunteerComponent,
     UpdateComponent,
-
-   
-   
-  
-=======
     VolunteerProfileComponent,
     medicineComponent,
     PatientComponentComponent,
     HomeComponent,
     PatientComponent,
->>>>>>> e7a3f3b503a5e201e706cf58f9860de2facd41c7
-
   ],
   imports: [
     BrowserModule,
@@ -57,23 +42,14 @@ import { PatientComponent } from './consultation/patient/patient.component';
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
-<<<<<<< HEAD
-
-
-    ReactiveFormsModule,  
-    HttpClientModule,
-
-    ReactiveFormsModule,
-
-
     ReactiveFormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-
-=======
->>>>>>> e7a3f3b503a5e201e706cf58f9860de2facd41c7
+    ReactiveFormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
-  providers: [HttpClientModule,HttpClient],
+  providers: [HttpClientModule, HttpClient],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
