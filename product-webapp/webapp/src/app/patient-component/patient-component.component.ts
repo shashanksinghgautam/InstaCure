522b48b9d1778cab91901b77b29410fd16887e4f
Karthika-Rajesh <karthikarajesh006@gmail.com>
2022-08-11 20:41:26 +0530

    Validation update

--- product-webapp/webapp/src/app/patient-component/patient-component.component.ts
+++ product-webapp/webapp/src/app/patient-component/patient-component.component.ts
@@ -1,25 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-patient-component',
   templateUrl: './patient-component.component.html',
   styleUrls: ['./patient-component.component.css']
 })
-export class PatientComponentComponent implements OnInit {
+export class PatientComponentComponent implements OnInit{
 
-  constructor() { }
-  patForm!: FormGroup;
-  
-  pat = {email: ""};
+  email:string = "qwe@gmail.com";
+
+  @ViewChild("patform") public formref!: NgForm;
+  constructor(private router:Router) { }
+
   ngOnInit(): void {
-    this.patForm = new FormGroup({
-      email: new FormControl(this.pat.email, [
-        Validators.required,
-      ])
-    });
   }
 
+  validate(ref: any):void{
+    console.log("success" + ref.mob.value);//ref.email.value +"  "+ ref.mob.value);
+    //this.router.navigateByUrl('dashboard')
+   }
   
 
 }
