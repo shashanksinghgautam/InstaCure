08a0a45fae0ae5bc99b10df0ff6865ab44d19e80
rupalimehra7018029157 <rupalimehra7018029157@gmail.com>
2022-08-18 22:48:22 +0530

    initial commit

--- doctor-service/src/main/java/com/stackroute/doctorservice/controller/DoctorController.java
+++ doctor-service/src/main/java/com/stackroute/doctorservice/controller/DoctorController.java
@@ -1,4 +1,35 @@
 package com.stackroute.doctorservice.controller;
 
+import com.stackroute.doctorservice.model.DoctorProfile;
+import com.stackroute.doctorservice.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/doctor")
+@CrossOrigin(origins = "http://localhost:3366")
 public class DoctorController {
+    @Autowired
+    private DoctorService service;
+
+    @PostMapping("/add")
+    public ResponseEntity<DoctorProfile> addDetails(@RequestBody DoctorProfile doctor) {
+        DoctorProfile user = this.service.addNew(doctor);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+    @GetMapping("/get")
+    public List<DoctorProfile> getAllDoctors() {
+        return this.service.getAll();
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<DoctorProfile> get(@PathVariable(value = "id") int id) {
+        DoctorProfile doctor = this.service.getById(id);
+        return new ResponseEntity<>(doctor, HttpStatus.OK);
+    }
+
 }
--- doctor-service/src/main/java/com/stackroute/doctorservice/entity/DoctorProfile.java
+++
@@ -1,4 +0,0 @@
-package com.stackroute.doctorservice.entity;
-
-public class DoctorProfile {
-}
